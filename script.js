4a81487b16c642669b264588eb1aac6363d51ab9
Daniel Favell <danfavell97@gmail.com>
2024-02-07 11:09:34 +0000

    added javascript to change the text on answer buttons and update score if the answer is correct

--- script.js
+++ script.js
@@ -1,40 +1,71 @@
 const submitButton = document.querySelector("#submit");
-const questionArray = [["this a test question"],["I wonder if this works"]];
+const questionArray = [
+    {
+        question: "this a test question",
+        correctAnswer: "testcorrect",
+        possibleAnswers: ["testcorrect", "testincorrect", "why", "hope it works"],
+    },
+    {
+        question: "I wonder if this works",
+        correctAnswer: "testcorrect",
+        possibleAnswers: ["testcorrect", "does it?", "how?", "hope so"],
+    },
+    {
+        question: "What is the capital of England?",
+        correctAnswer: "London",
+        possibleAnswers: ["London", "Paris", "Cardiff", "Berlin"],
+    },
+];
 let score = 0;
+let randomQuestion = null;
+const answerButtons = document.querySelectorAll(".answers-button");
+let selectedAnswer = null;
 
 function pageLoaded() {
-    submitButton.addEventListener("click", submitAnswer)
+    submitButton.addEventListener("click", submitAnswer);
+    answerButtons.forEach(item => {
+        item.addEventListener("click", updateSelectedAnswer)
+    });
+    newQuestion();
 }
 
 window.onload = pageLoaded();
 
-function increaseScore(){
-score += 1
+function increaseScore() {
+    score += 1;
 }
 
-function decreaseScore(){
-    score -= 1
+function decreaseScore() {
+    score -= 1;
 }
 
-function submitAnswer(){
-const correctAnswer = null;
-const selectedAnswer = null;
-if (correctAnswer === selectedAnswer){
-    increaseScore();
-}else{
-    decreaseScore();
-}
-newQuestion()
+function submitAnswer() {
+    if (randomQuestion.correctAnswer === selectedAnswer) {
+        increaseScore();
+    } else {
+        decreaseScore();
+    }
+    newQuestion();
 }
 
-
-
-function newQuestion(){
+function newQuestion() {
     const questionText = document.querySelector("#question-text");
-    const randomQuestion = questionArray[randomNumber()];
-    questionText.textContent = randomQuestion[0]
+    randomQuestion = questionArray[randomQuestionFromArray()];
+    questionText.textContent = randomQuestion.question;
+    generateAnswers();
+    selectedAnswer = null;
 }
 
-function randomNumber(){
-   return Math.floor(Math.random() * questionArray.length)
+function randomQuestionFromArray() {
+    return Math.floor(Math.random() * questionArray.length);
+}
+
+function generateAnswers() {
+    for (let i = 0; i < randomQuestion.possibleAnswers.length; i++) {
+            answerButtons[i].textContent = randomQuestion.possibleAnswers[i];
+    }
+}
+
+function updateSelectedAnswer(){
+selectedAnswer = this.innerText;
 }
