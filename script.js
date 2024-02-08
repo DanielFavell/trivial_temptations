dc64b25d784d75193dac0eb4866a7f3546af468e
Daniel Favell <danfavell97@gmail.com>
2024-02-08 12:26:07 +0000

    made message show up to say how well you did

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let score = document.querySelector("#scoreNumber");
 let currentQuestion = null;
 const answerButtons = document.querySelectorAll(".answers-button");
 let selectedAnswer = null;
+let questionNumber = 0
+let gameEnded = false;
 
 async function pageLoaded() {
     submitButton.addEventListener("click", submitAnswer);
@@ -41,12 +43,25 @@ function saveInfo(info) {
 window.onload = pageLoaded();
 
 setInterval(() => {
-    if (Number(document.querySelector('#timer').textContent) > 0) {
-        let timeRemaining = Number(document.querySelector('#timer').textContent);
-        timeRemaining--
-        document.querySelector('#timer').textContent = timeRemaining;
-    } else {
-        submitAnswer()
+    if (gameEnded) { 
+        document.getElementById("submit").setAttribute("disabled", "");
+        if (score.textContent === "10"){
+        document.querySelector(".results").innerHTML = `<span class="perfect">Perfect</span> You answered 10 From 10`
+        }
+        else if(Number(score.textContent) >= 5){
+            document.querySelector(".results").innerHTML = `<span class="good">Good</span> You have answered more than 5 From 10`
+        }else{
+            document.querySelector(".results").innerHTML = `<span class="try_again">Try Again</span> You have answered less than 5 From 10`
+        }
+    }
+    else {
+        if (Number(document.querySelector('#timer').textContent) > 0) {
+            let timeRemaining = Number(document.querySelector('#timer').textContent);
+            timeRemaining--
+            document.querySelector('#timer').textContent = timeRemaining;
+        } else {
+            submitAnswer()
+        }
     }
 }, 1000);
 
@@ -54,24 +69,30 @@ function increaseScore() {
     score.textContent = Number(score.textContent) + 1;
 }
 
-function decreaseScore() {
-    score.textContent = Number(score.textContent) - 1;
-}
+// function decreaseScore() {
+//     // score.textContent = Number(score.textContent) - 1;
+// }
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key].trim() === value);
 }
 function submitAnswer() {
     if (currentQuestion.correct_answer === getKeyByValue(currentQuestion.options, selectedAnswer)) {
         increaseScore();
-    } else {
-        decreaseScore();
+    }
+    // else {
+    //     decreaseScore();
+    // }
+    questionNumber++
+    document.querySelector(".off").classList = "on"
+    if (questionNumber === 9) {
+        gameEnded = true;
     }
     newQuestion();
 }
 
 function newQuestion() {
     const questionText = document.querySelector("#question-text");
-    currentQuestion = questionArray[randomNumberGenerator()];
+    currentQuestion = questionArray[questionNumber];
 
     questionText.textContent = currentQuestion.question;
     generateAnswers();
@@ -79,9 +100,9 @@ function newQuestion() {
     document.querySelector('#timer').textContent = "10"
 }
 
-function randomNumberGenerator() {
-    return Math.floor(Math.random() * questionArray.length);
-}
+// function randomNumberGenerator() {
+//     return Math.floor(Math.random() * questionArray.length);
+// }
 
 function generateAnswers() {
     answerButtons[0].textContent = currentQuestion.options.a;
