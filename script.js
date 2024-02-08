a03650a922ba349d239a8c6b01c1667edcc60c15
Daniel Favell <danfavell97@gmail.com>
2024-02-08 12:07:25 +0000

    generate questions from json file

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
 const submitButton = document.querySelector("#submit");
-const questionArray = [
-    {
-        question: "this a test question",
-        correctAnswer: "testcorrect",
-        possibleAnswers: ["testcorrect", "testincorrect", "why", "hope it works"],
-    },
-    {
-        question: "I wonder if this works",
-        correctAnswer: "testcorrect",
-        possibleAnswers: ["testcorrect", "does it?", "how?", "hope so"],
-    },
-    {
-        question: "What is the capital of England?",
-        correctAnswer: "London",
-        possibleAnswers: ["London", "Paris", "Cardiff", "Berlin"],
-    },
-];
+const questionArray = [];
+let questionObject = [];
+let possibleQuestions = [];
 let score = document.querySelector("#scoreNumber");
-let randomQuestion = null;
+let currentQuestion = null;
 const answerButtons = document.querySelectorAll(".answers-button");
 let selectedAnswer = null;
 
-function pageLoaded() {
+async function pageLoaded() {
     submitButton.addEventListener("click", submitAnswer);
     answerButtons.forEach(item => {
         item.addEventListener("click", updateSelectedAnswer)
     });
+
+    questionObject = await generateQuestionArray();
+    questionObject = questionObject.questions;
+
+
+    while (possibleQuestions.length < 10) {
+        possibleQuestions.push(Math.floor(Math.random() * 31))
+    }
+
+    for (let i = 0; questionArray.length < 10; i++) {
+        questionArray.push(questionObject[possibleQuestions[i]])
+    }
     newQuestion();
+}
+
+async function generateQuestionArray() {
+    let temp;
+    const res = await fetch("html_questions.json")
+    temp = await res.json();
+    return temp
+}
+
+function saveInfo(info) {
+    questionObject = info;
 }
 
 window.onload = pageLoaded();
 
-setInterval(() => { 
-    if(Number(document.querySelector('#timer').textContent) > 0){
-    let timeRemaining = Number(document.querySelector('#timer').textContent);
-    timeRemaining--
-    document.querySelector('#timer').textContent = timeRemaining;
-    }else{
+setInterval(() => {
+    if (Number(document.querySelector('#timer').textContent) > 0) {
+        let timeRemaining = Number(document.querySelector('#timer').textContent);
+        timeRemaining--
+        document.querySelector('#timer').textContent = timeRemaining;
+    } else {
         submitAnswer()
     }
-  }, 1000);
+}, 1000);
 
 function increaseScore() {
-    score.textContent = Number(score.textContent)+1;
+    score.textContent = Number(score.textContent) + 1;
 }
 
 function decreaseScore() {
-    score.textContent = Number(score.textContent)-1;
+    score.textContent = Number(score.textContent) - 1;
 }
-
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key].trim() === value);
+}
 function submitAnswer() {
-    if (randomQuestion.correctAnswer === selectedAnswer) {
+    if (currentQuestion.correct_answer === getKeyByValue(currentQuestion.options, selectedAnswer)) {
         increaseScore();
     } else {
         decreaseScore();
@@ -60,24 +71,25 @@ function submitAnswer() {
 
 function newQuestion() {
     const questionText = document.querySelector("#question-text");
-    randomQuestion = questionArray[randomQuestionFromArray()];
-    questionText.textContent = randomQuestion.question;
+    currentQuestion = questionArray[randomNumberGenerator()];
+
+    questionText.textContent = currentQuestion.question;
     generateAnswers();
     selectedAnswer = null;
     document.querySelector('#timer').textContent = "10"
 }
 
-function randomQuestionFromArray() {
+function randomNumberGenerator() {
     return Math.floor(Math.random() * questionArray.length);
 }
 
 function generateAnswers() {
-    let randomOffset = Math.floor(Math.random() * 4);
-    for (let i = 0; i < randomQuestion.possibleAnswers.length; i++) {
-            answerButtons[(i+randomOffset)%4].textContent = randomQuestion.possibleAnswers[i];
-    }
+    answerButtons[0].textContent = currentQuestion.options.a;
+    answerButtons[1].textContent = currentQuestion.options.b;
+    answerButtons[2].textContent = currentQuestion.options.c;
+    answerButtons[3].textContent = currentQuestion.options.d;
 }
 
-function updateSelectedAnswer(){
-selectedAnswer = this.innerText;
+function updateSelectedAnswer() {
+    selectedAnswer = this.innerText;
 }
